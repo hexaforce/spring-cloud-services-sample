f9575f19899827cda085b204c0c88a1e63bf894d
Tomokazu Tantaka <tantaka.tomokazu@gmai.com>
2018-04-15 13:43:05 +0900

    aa

---
+++ basic-services/src/main/java/microservices/cloud/basic/services/App.java
@@ -0,0 +1,13 @@
+package microservices.cloud.basic.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
